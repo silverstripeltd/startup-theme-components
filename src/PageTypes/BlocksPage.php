0529fc4f59f0dbfca5cf10db095e2bdb2c24bc8f
Annie Kyles <annakyles@gmail.com>
2024-09-30 11:22:35 +1300

    SDEMO-122: WIP

--- src/PageTypes/BlocksPage.php
+++ src/PageTypes/BlocksPage.php
@@ -3,17 +3,23 @@
 namespace SilverStripe\StartupThemeComponents\PageTypes;
 
 use Page;
-// use SilverStripe\StartupThemeComponents\Extensions\HeroExtension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\RequiredFields;
 
 class BlocksPage extends Page
 {
     private static string $table_name = 'BlocksPage';
 
-    // private static array $extensions = [
-    //     HeroExtension::class,
-    // ];
+    private static array $db = [
+        'HeroTitle' => 'Varchar(255)',
+        'ShowHero' => 'Boolean',
+    ];
 
+    private static array $defaults = [
+        'ShowHero' => true,
+    ];
 
     /**
      * @return RequiredFields
@@ -21,7 +27,21 @@ class BlocksPage extends Page
     public function getCMSValidator(): RequiredFields
     {
         return RequiredFields::create([
-            'Title',
+            'HeroTitle',
+        ]);
+    }
+
+    protected function buildMetaAreaFields(FieldList $fields): void
+    {
+        parent::buildMetaAreaFields($fields);
+
+        $fields->addFieldToTab(
+            'Root.Main',
+            $metaTitle = TextField::create('HeroTitle', $this->fieldLabel('HeroTitle')),
+            'MenuTitle'
+        );
+
+        $fields->removeByName([
         ]);
     }
 }
--- themes/startup-theme-components/templates/BlocksPage.ss
+++
@@ -1,2 +0,0 @@
-Elemental area: 
-$ElementalArea
