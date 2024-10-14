7351f1b7d59d33eb4ab97f2cb058f90d2df529cb
Brooke Lord <brooke.lord@silverstripe.com>
2024-10-14 19:42:37 +1300

    SDEMO-708: Make field read only when not in edit screen

--- src/Extensions/FileFormFactoryExtension.php
+++ src/Extensions/FileFormFactoryExtension.php
@@ -2,6 +2,7 @@
 
 namespace SilverStripe\StartupTheme;
 
+use SilverStripe\AssetAdmin\Forms\AssetFormFactory;
 use SilverStripe\Forms\Tip;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
@@ -12,29 +13,35 @@ class FileFormFactoryExtension extends DataExtension
 {
     public function updateFormFields(FieldList $fields, $controller, $name, $context)
     {
-        $record = $context['Record'];
+        $record = $context['Record'] ?? null;
+        $type = $context['Type'] ?? null;
 
-        if ($record->appCategory() === 'image') {
-            $fields->insertAfter(
-                'Title',
-                $altTextField = TextField::create(
-                    'AltText',
-                    _t('SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltText', 'Alternative text (alt)')
-                )
-            );
-
-            $altTextDescription = _t(
-                'SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltTextTip',
-                'Description for visitors who are unable to view the image (using screenreaders or ' .
-                'image blockers). Recommended for images which provide unique context to the content.'
-            );
-
-            if ($altTextField instanceof TippableFieldInterface) {
-                $altTextField->setTip(new Tip($altTextDescription, Tip::IMPORTANCE_LEVELS['HIGH']));
-            } else {
-                $altTextField->setDescription($altTextDescription);
-            }
+        if (!$record || $record->appCategory() !== 'image') {
+            return;
         }
+
+        $altTextField = TextField::create(
+            'AltText',
+            _t('SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltText', 'Alternative text (alt)')
+        );
+
+        if ($type) {
+            $altTextField = $altTextField->performReadonlyTransformation();
+        }
+
+        $altTextDescription = _t(
+            'SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.AltTextTip',
+            'Description for visitors who are unable to view the image (using screenreaders or ' .
+            'image blockers). Recommended for images which provide unique context to the content.'
+        );
+
+        if ($altTextField instanceof TippableFieldInterface) {
+            $altTextField->setTip(new Tip($altTextDescription, Tip::IMPORTANCE_LEVELS['HIGH']));
+        } else {
+            $altTextField->setDescription($altTextDescription);
+        }
+
+        $fields->insertAfter('Title', $altTextField);
     }
 
 }
